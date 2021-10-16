0396d4e14d4028f2e57bc38c9ea84b2684dd4060
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2021-10-17 02:43:10 +0300

    11
    11

--- task-1.js
+++ task-1.js
@@ -1,12 +1,51 @@
+// const delay = ms => {
+//     return new Promise((resolve) => {
+//       setTimeout(() => { resolve(ms) }, ms);
+//     });
+//   };
+  
+//   const logger = time => console.log(`Resolved after ${time}ms`);
+  
+//   // Вызовы функции для проверки
+//   delay(2000).then(logger); // Resolved after 2000ms
+//   delay(1000).then(logger); // Resolved after 1000ms
+//   delay(1500).then(logger); // Resolved after 1500ms
+
+const makePromise = () => {
+  return new Promise((resolve, reject) => {
+    resolve ('resolve!')
+  });
+};
+
+// makePromise().then(result => console.log(result));
+
 const delay = ms => {
-    return new Promise((resolve) => {
-      setTimeout(() => { resolve(ms) }, ms);
-    });
-  };
+  return new Promise((resolve, reject) => {
+    const passed = Math.random() > 0.3;
+    setTimeout(() => {
+      if(passed){
+        resolve(ms)}
+        reject('error!')
+      }, ms)
+  })
   
-  const logger = time => console.log(`Resolved after ${time}ms`);
-  
-  // Вызовы функции для проверки
-  delay(2000).then(logger); // Resolved after 2000ms
-  delay(1000).then(logger); // Resolved after 1000ms
-  delay(1500).then(logger); // Resolved after 1500ms
+
+  // Твой код
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+delay(2000).then(logger).catch(error => console.log(error)); // Resolved after 2000ms
+delay(1000).then(logger).catch(error => console.log(error)); // Resolved after 1000ms
+delay(1500).then(logger).catch(error => console.log(error)); // Resolved after 1500ms
+console.log(`%c Hello!`,
+'color: tomato; font-size: 54px');
+// const promise = new Promise((resolve, reject) => {
+//   /*
+//    * Эта функция будет вызвана автоматически. В ней можно выполнять
+//    * любые асинхронные операции. Когда они завершатся — нужно
+//    * вызвать одно из: resolve(результат) при успешном выполнении,
+//    * или reject(ошибка) при ошибке.
+//    */
+// });
--- task-2.js
+++ task-2.js
@@ -17,3 +17,40 @@ const users = [
   
   toggleUserState(users, 'Mango').then(logger);
   toggleUserState(users, 'Lux').then(logger);
+  
+
+  // Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис.
+
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+
+// const toggleUserState = (allUsers, userName) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+// return new Promise(resolve => {
+// resolve (updatedUsers);
+// })
+// };
+
+// const logger = updatedUsers => console.table(updatedUsers);
+
+// /*
+//  * Сейчас работает так
+//  */
+// // toggleUserState(users, 'Mango', logger);
+// // // toggleUserState(users, 'Lux', logger);
+// // toggleUserState(users, 'Ajax', logger);
+
+// /*
+//  * Должно работать так
+//  */
+// toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
+// toggleUserState(users, 'Poly').then(logger);
+// toggleUserState(users, 'Ajax').then(logger);
+
